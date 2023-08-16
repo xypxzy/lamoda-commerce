f886749fb50c1b60542bbb984c4d3d6f5f0b004d
root <xypxzy@gmail.com>
2023-08-16 20:35:37 +0600

    add routing to Catalog page, fix favorite button

--- src/components/Products/Products.tsx
+++ src/components/Products/Products.tsx
@@ -1,19 +1,20 @@
 import ProductCard from "../ProductCard/ProductCard.tsx";
 import cls from './Products.module.css'
+
 const Products = () => {
     return (
         <section className={cls.products}>
             <div className={cls.productsContainer}>
                 <h1 className={cls.productsTitle}>Каталог</h1>
                 <div className={cls.productsWrapper}>
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
+                    <ProductCard/>
+                    <ProductCard/>
+                    <ProductCard/>
+                    <ProductCard/>
+                    <ProductCard/>
+                    <ProductCard/>
+                    <ProductCard/>
+                    <ProductCard/>
                 </div>
             </div>
         </section>
