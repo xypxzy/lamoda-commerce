125238ac02c63b33230ce6d963eb7fa98c2e38e4
grauverg <v.n.grauverg@gmail.com>
2023-08-16 18:30:18 +0600

    Added swagger

--- django-api/core/urls.py
+++ django-api/core/urls.py
@@ -2,10 +2,31 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
+
+schema_view = get_schema_view(
+openapi.Info(
+        title="Lamoda API",
+        default_version='1',
+        description="API for a web app",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email=""),
+        license=openapi.License(name=""),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
+
+swagger_urlpatterns = [
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0)),
+]
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('products/', include('backend.products.urls')),
     path('users/', include('backend.users.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # adding the address of media folder
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + swagger_urlpatterns
+# adding swagger, static and media roots
