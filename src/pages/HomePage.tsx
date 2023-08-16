3d6a1946e796e9c87b25a22a330e9a7efc97b63e
Emil Ashirbekov <emkaashirbekov7910a@gmail.com>
2023-08-17 02:22:07 +0600

    finished brands section

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Hero from "../components/Hero";
+import Brands from "../components/Brands";
+
+const HomePage: React.FC = () => {
+  return (
+    <>
+      <Hero />
+      <Brands />
+    </>
+  );
+};
+
+export default HomePage;
