17caa23e979417c4a860991d527c9b8c35b43b6d
root <xypxzy@gmail.com>
2023-08-17 11:19:14 +0600

    add accordion, add ProductDetails in routing

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -2,6 +2,7 @@ import {Route, Routes} from "react-router-dom";
 import Layout from "../layout/Layout.tsx";
 import {Counter} from "../components/Counter.tsx";
 import CatalogPage from "../pages/CatalogPage/CatalogPage.tsx";
+import ProductDetails from "../components/ProductDetails/ProductDetails.tsx";
 
 export default function Root() {
     return (
@@ -9,6 +10,7 @@ export default function Root() {
             <Route path={'/'} element={<Layout/> }>
                 <Route path={'counter'}  element={<Counter />} />
                 <Route path={'catalog'} element={<CatalogPage />} />
+                <Route path={'product/:id'} element={<ProductDetails />} />
             </Route>
         </Routes>
     )
