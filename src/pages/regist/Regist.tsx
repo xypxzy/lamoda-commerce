d8f0e8f35beb9b1ec1d91a9ebf63df8b33e2cc16
Ayanchi <ayanausub13@gmail.com>
2023-08-17 20:38:33 +0600

    created auth without api req

---
+++ src/pages/regist/Regist.tsx
@@ -0,0 +1,123 @@
+import { useForm } from "react-hook-form"
+import { Link } from "react-router-dom"
+import styles from './Regist.module.scss'
+import person from '../../assets/person_filled.svg'
+import mail from '../../assets/mail.svg'
+import lock from '../../assets/lock.svg'
+import eye from '../../assets/eye.svg'
+import eyeblock from '../../assets/eyeslash.svg'
+import { useState } from "react"
+
+const Regist = () => {
+    const {register, handleSubmit, formState: { errors }} = useForm();
+    const [booleanPassword, setBooleanPassword] = useState(true); 
+    const [booleanConfirmPassword, setBooleanConfirmPassword] = useState(true); 
+
+
+    return(
+        <section>
+            <h1>Регистрация</h1>
+            <form action="POST" onSubmit={handleSubmit()}>
+                <div className="mb-6">
+                    <img src={person} />
+                    <input 
+                        type="name" 
+                        id="name" 
+                        placeholder="name"
+                        {...register('name', {
+                            required: "Параметр обязателен",
+                            maxLength: {
+                                value: 15,
+                                message: 'Ваше имя должно быть меньше 20 символов'
+                            },
+                            minLength: {
+                                value: 3,
+                                message: 'Ваше имя должно быть больше 3 символов'
+                            },
+                        })}
+                        ></input>
+                        {errors.name && <span className="error" role="alert"></span>}
+                </div>
+                <div className="mb-6">
+                    <img src={mail} alt="" />
+                    <input 
+                        type="email" 
+                        id="email" 
+                        placeholder="john.doe@company.com"
+                        {...register('email', {
+                            required: "Параметр обязателен",
+                            maxLength: {
+                                value: 15,
+                                message: 'Ваше имя должно быть меньше 20 символов'
+                            },
+                            minLength: {
+                                value: 3,
+                                message: 'Ваше имя должно быть больше 3 символов'
+                            },
+                        })}
+                    ></input>
+                </div>
+                <div className="mb-6">
+                    <img src={lock} alt="" />
+                    <input 
+                        type={booleanPassword ? "password" : "text"}
+ 
+                        id="password" 
+                        placeholder="•••••••••"
+                        {...register('password', {
+                            required: "Параметр обязателен",
+                            maxLength: {
+                                value: 15,
+                                message: 'Ваше имя должно быть меньше 20 символов'
+                            },
+                            minLength: {
+                                value: 3,
+                                message: 'Ваше имя должно быть больше 3 символов'
+                            },
+                        })}
+                    ></input>
+                    {booleanPassword ? (
+                        <img src={eye} alt="" className={styles.imgEye} onClick={() => setBooleanPassword(false)}/>
+                        ) : (
+                            <img src={eyeblock} alt="" className={styles.imgEye} onClick={() => setBooleanPassword(true)}/>
+                        )
+                    }
+                    
+                </div>
+                <div className="mb-6">
+                    <img src={lock} alt="" />
+                    <input 
+                        type={booleanConfirmPassword ? "password" : "text"}
+                        id="confirm_password" 
+                        placeholder="•••••••••"
+                        {...register('confirm_password', {
+                            required: "Параметр обязателен",
+                            maxLength: {
+                                value: 15,
+                                message: 'Ваше имя должно быть меньше 20 символов'
+                            },
+                            minLength: {
+                                value: 3,
+                                message: 'Ваше имя должно быть больше 3 символов'
+                            },
+                        })}
+                    ></input>
+                    {booleanConfirmPassword ? (
+                        <img src={eye} alt="" className={styles.imgEye} onClick={() => setBooleanConfirmPassword(false)}/>
+                        ) : (
+                            <img src={eyeblock} alt="" className={styles.imgEye} onClick={() => setBooleanConfirmPassword(true)}/>
+                        )
+                    }
+                    
+                </div>
+
+
+                <button type="submit" className="text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:outline-none max-w-md focus:ring-gray-300 font-medium rounded-lg px-5 py-2.5 text-center dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800">Зарегистрироваться</button>
+            </form>
+
+            <Link to='/login' className="font-medium text-blue-600 dark:text-blue-500 hover:underline">есть аккаунт</Link>
+
+        </section>
+    )
+}
+export default Regist
--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,13 +1,27 @@
 import {Route, Routes} from "react-router-dom";
 import Layout from "../layout/Layout.tsx";
 import {Counter} from "../components/Counter.tsx";
+import Regist from "../pages/regist/Regist.tsx";
+import Login from "../pages/login/Login.tsx";
+
 
 export default function Root() {
+    const checkAuth = true
     return (
         <Routes>
-            <Route path={'/'} element={<Layout/> }>
-                <Route path={'counter'}  element={<Counter />} />
-            </Route>
+            {checkAuth ? (
+                <>
+                    <Route path="/registration" element={<Regist/>} />
+                    <Route path="login" element={<Login/>}/>
+                </>
+            ) : (
+                <>
+                    <Route path={'/'} element={<Layout/> }>
+                        <Route path={'counter'}  element={<Counter />} />
+                    </Route>
+                </>
+            )}
+            
         </Routes>
     )
 }
