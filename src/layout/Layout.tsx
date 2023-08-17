a3585383af1678c022625d329a5f2e46deb89d07
Emil Ashirbekov <emkaashirbekov7910a@gmail.com>
2023-08-17 11:27:31 +0600

    finished footer

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,12 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header/index.tsx";
+import Footer from "../components/Footer/index.tsx";
 
 const Layout = () => {
   return (
     <div>
       <Header />
       <Outlet />
-      {/*//Footer*/}
+      <Footer />
     </div>
   );
 };
