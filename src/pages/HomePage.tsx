ff39ba36b1301a02db7bfc7085a8bae9049a5295
Emil Ashirbekov <emkaashirbekov7910a@gmail.com>
2023-08-20 21:09:16 +0600

    added features component

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Hero from "../components/Hero";
 import Brands from "../components/Brands";
+import NewCollection from "../components/NewCollection/NewCollection";
+import Features from "../components/Features/Features";
 
 const HomePage: React.FC = () => {
   return (
     <>
       <Hero />
+      <NewCollection />
+      <Features />
       <Brands />
     </>
   );
