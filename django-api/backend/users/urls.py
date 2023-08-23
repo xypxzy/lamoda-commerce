cfe338d61d959e7d18335f3977ab6b2d3f4d392e
grauverg <v.n.grauverg@gmail.com>
2023-08-23 16:02:01 +0600

    Added JWT authentication

--- django-api/backend/users/urls.py
+++ django-api/backend/users/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path, include
-from rest_framework.authtoken.views import obtain_auth_token
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from . import views
@@ -7,11 +6,11 @@ from backend.products.views import FavouriteProductListAPIView, FavouriteProduct
 
 urlpatterns = [
     path('register/', views.UserRegisterAPIView.as_view()),
-    # token obtaining
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-
     path('auth/', include('rest_framework.urls')),
+
+    # jwt token
+    path('token/', TokenObtainPairView.as_view(), name='token_pair_obtain'), 
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'), 
 
     path('', views.UserRetrieveUpdateAPIView.as_view()),
     path('favourite/', FavouriteProductListAPIView.as_view()),
