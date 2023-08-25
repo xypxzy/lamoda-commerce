a8cd4a8f1fd67611f66b1df6483088b9e412e040
Emil Ashirbekov <emkaashirbekov7910a@gmail.com>
2023-08-25 18:35:07 +0600

    minimized the images sizes and fixed new collection component

--- src/components/NewCollection/index.tsx
+++ src/components/NewCollection/index.tsx
@@ -3,34 +3,27 @@ import styles from "./NewCollection.module.css";
 import { Link } from "react-router-dom";
 import { useGetNewProductsQuery } from "../../store/products/productsApi";
 
-const NewCollection = () => {
+const NewCollection: React.FC = () => {
   const { data = [], error, isLoading } = useGetNewProductsQuery(3);
-  console.log(data)
   return (
     <section id="newcollection" className={styles.collection}>
       <h2 className="mb-20 font-bold text-3xl">Новая коллекция</h2>
-      {data.map((el, index) => (
-        <>
-          <div className={styles.col_grid}>
-            <div className={styles.col_card}>
+      <div className={styles.col_grid}>
+        {data.slice(0, 3).map((el) =>
+          el.images.map((obj, index) => (
+            <div key={index} className={styles.col_card}>
               <Link to="/">
-                
-                  <img
-                  className="mb-5"
-                  src={el.images[index].image}
-                  alt="collection img"
-                />
-              
+                <img className="mb-5" src={obj.image} alt="collection img" />
               </Link>
               <div className={styles.col_info}>
                 <h3 className={styles.col_title}>Mini Watermelon Glow PHA</h3>
                 <p className={styles.col_price}>$15.00</p>
               </div>
             </div>
-          </div>
-        </>
-      ))}
-      
+          ))
+        )}
+      </div>
+
       <div className={styles.col_desc}>
         <Link rel="noopener noreferrer" to="catalog" className={styles.col_btn}>
           Перейти в каталог
