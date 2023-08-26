d7ebe0fb9296aef4eaa024ec943571924816a648
valentine <mokrousov.va@yandex.ru>
2023-08-26 18:07:48 +0600

    Fix in user/, added default_admin

--- django-api/backend/users/views.py
+++ django-api/backend/users/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from django.contrib.auth import get_user_model
 from rest_framework.generics import get_object_or_404
@@ -20,17 +21,17 @@ class UserRegisterAPIView(generics.CreateAPIView):
 class UserRetrieveUpdateAPIView(generics.RetrieveUpdateAPIView):
     queryset = User.objects.all()
     serializer_class = serializers.UserSerializer
-    permission_classes = [IsAuthor, ]
+    permission_classes = [IsAuthenticated, ]
 
-    
+
     def get_object(self):
         """
-        Returns user instance so user doesn't have to search for his id. 
+        Returns user instance so user doesn't have to search for his id.
         """
 
-        # getting current logged in user instance 
+        # getting current logged in user instance
         obj = get_object_or_404(self.queryset, id=self.request.user.id)
         # checking user's permissions
-        self.check_object_permissions(self.request, obj)
+        # self.check_object_permissions(self.request, obj)
         return obj
     
--- django-api/entrypoint.sh
+++ django-api/entrypoint.sh
@@ -13,8 +13,8 @@ fi
 
 python manage.py flush --no-input
 python manage.py migrate
-python manage.py createsuperuser --noinput --username admin --email admin@example.com
+python manage.py create_default_admin
 # python manage.py loaddata backend/products/fixtures/goods_w.json
 # python manage.py loaddata backend/products/fixtures/images_w.json
 
-exec "$@"
+
