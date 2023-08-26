305def1c74be685059baa5163fb2ac091f2a62ee
xypxzy <xypxzy@gmail.com>
2023-08-26 10:51:51 +0600

    add landing register page

--- src/store/auth/authApi.ts
+++ src/store/auth/authApi.ts
@@ -1,39 +1,56 @@
-
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/dist/query/react";
+import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/dist/query/react";
 
 export interface AuthResponse {
-    access: string;
-    refresh: string;
-};
+  access: string;
+  refresh: string;
+}
+
 interface TokenInputProps {
-    username: string;
-    password: string;
-};
+  username: string;
+  password: string;
+}
+
+export interface RegisterProps {
+  username: string,
+  password: string,
+  password2: string,
+  first_name: string,
+  last_name: string,
+  email: string
+}
+
+export interface RegisterResponse {
+  username: string,
+  first_name: string,
+  last_name: string,
+  email: string,
+  profile_image: string,
+}
 
 export const authApi = createApi({
-    reducerPath: 'authApi',
-    baseQuery: fetchBaseQuery({baseUrl: 'http://localhost:8002/users'}),
-    endpoints: (builder) => ({
-        addAuth: builder.mutation({
-            query: (body) => ({
-                url: '/register/',
-                method: 'POST',
-                body
-            }),
-        }),
-        addToken: builder.mutation<AuthResponse, Partial<TokenInputProps>>({
-            query: (credentials) => {
-                return ({
-                    url: '/token/',
-                    method: 'POST',
-                    body: credentials
-                })
-            },
-        }),
-    })
+  reducerPath: 'authApi',
+  baseQuery: fetchBaseQuery({baseUrl: 'http://localhost:8002/users'}),
+  endpoints: (builder) => ({
+    addAuth: builder.mutation({
+      query: (body) => ({
+        url: '/register/',
+        method: 'POST',
+        body
+      }),
+    }),
+    addToken: builder.mutation<AuthResponse, Partial<TokenInputProps>>({
+      query: (credentials) => {
+        return ({
+          url: '/token/',
+          method: 'POST',
+          body: credentials
+        })
+      },
+    }),
+  })
 })
 
 export const {
-    useAddAuthMutation,
-    useAddTokenMutation
-  } = authApi;
+  useAddAuthMutation,
+  useAddTokenMutation
+} = authApi;
